5a8baa5910f8d03c84be3b36284f74b2f3eedd46
unknown <m.hadi.jamshidi@gmail.com>
2020-03-27 14:54:36 +0430

    debug=False

--- pi/settings.py
+++ pi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&ei90zy85dvh719zfa#-qo_$4_y(*dm)&tc48d^)1+!9*6k5(8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
